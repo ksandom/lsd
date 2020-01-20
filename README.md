0f85dc153cf5a95c354b03822c58feece4118c8c
Kevin Sandom <kjsandom@gmail.com>
2020-01-20 02:28:30 +0100

    More robust detection, in more cases
    * Resolve symlinks.
    * Call --help is we can't figure it out from the header.
    * Lots of work arounds for variations.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ Listing descriptions of scripts. Eg
 
 ```bash
 $ ./lsd.sh
-lsd.sh  An ls style util for listing out the first-line descriptions.-
+lsd.sh  An ls style util for listing out the first-line descriptions.
 ```
 
 ```bash
-\$ ./lsd.sh /bin/zc*
-/bin/zcmp  Compare the uncompressed contents of compressed files, byte by byte.-
-/bin/zcat  Uncompress files to standard output.-
+$ ./lsd.sh /bin/zc*
+/bin/zcmp  Compare the uncompressed contents of compressed files, byte by byte.
+/bin/zcat  Uncompress files to standard output.
 ```
--- bin/lsd.sh
+++ bin/lsd.sh
@@ -9,21 +9,84 @@ function getFileList
 function inputToList
 {
     for fileName in "$@"; do
-        echo "$fileName"
+        if [ -d "$fileName" ]; then
+            ls -1 "$fileName" | sed "s#^#$fileName/#g"
+        else
+            echo "$fileName"
+        fi
     done
+}
+
+function convertSymlinks
+{
+    while read -r fileName; do
+        # TODO if [[ "$fileName" =~ ^.*:\ symbolic\ link.*$ ]]; then
+        if echo "$(file "$fileName")" | grep -q "^.*: symbolic link.*$"; then
+            echo "$(file "$fileName")" | sed 's/^.*symbolic link to //g'
+        else
+            echo "$fileName"
+        fi
+    done
+}
+
+function passesSanityChecks
+{
+    fileName="$1"
+    
+    if [ ! -e "$fileName" ]; then
+        echo "File not found"
+        return 1
+    elif [ -d "$fileName" ]; then
+        echo "Directory"
+        return 1
+    elif [ -h "$fileName" ]; then
+        echo "Unresolved symlink"
+        return 1
+    else
+        completeType="$(file "$fileName")"
+        
+        # TODO There are probably more types that should be supported here.
+        if [[ "$completeType" =~ ^.*ASCII.*$ ]]; then
+            echo "ASCII"
+            return 0
+        else
+            echo "Binary"
+            return 0
+        fi
+    fi
+}
+
+function exclude
+{
+    fileName="$1"
+    
+    # TODO Should we exclude lines mentioning \|$fileName ?
+    grep -iv "\(usage\| *or:\|^$\|^#!\|^ *$fileName\|^# *$\|Copyright\)"
 }
 
 function getDescriptionForFile
 {
     fileName="$1"
     
-    head -n10 "$fileName" | grep -v '\(^#!\|^# *$\|Copyright\)' | grep '^#' | cut -b3- | head -n 1
+    reason="$(passesSanityChecks "$fileName")"
+    if [ "$?" == 0 ] ; then
+        if [ "$reason" == 'ASCII' ]; then
+            head -n10 "$fileName" | exclude "$fileName" | grep '^#' | cut -b3- | head -n 1
+        else
+            # TODO Restrict to ELF?
+            # TODO Fix path to the fileName. Eg ./filename vs an absolute path that is passed.
+            $fileName --help 2>/dev/null | head | exclude "$fileName" | head -n1
+        fi
+    else
+        echo "$reason."
+    fi
+    
 }
 
 function glue
 {
     while read -r fileName; do
-        echo "${fileName}\0$(getDescriptionForFile "$fileName")"- &
+        echo "${fileName}\0$(getDescriptionForFile "$fileName")" &
     done
     wait
 }
@@ -34,7 +97,7 @@ function format
 }
 
 if [ "$1" == '' ]; then
-    getFileList | glue | format
+    getFileList | convertSymlinks | glue | format
 else
-    inputToList "$@" | glue | format
+    inputToList "$@" | convertSymlinks | glue | format
 fi
